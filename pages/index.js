74372897d894d746f1d3dc2c9b7fa0e9dd3d6eea
Shivam164 <shivam-ug20@nsut.ac.in>
2022-03-23 20:55:45 +0530

    done with first section of the homepage

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <title>Vadelabs</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,600&display=swap" rel="stylesheet"/>
 
       </Head>
